8f5874c9f95f742472fb89e8166d9a62bd34636f
Paul Pearce <chriton227@gmail.com>
2019-11-11 23:20:44 -0500

    Completed lesson 4

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 var app = new Vue({
     el: '#app',
     data: {
-        product:'Socks',
-        image:'vmSocks-green-onWhite.jpg',
-        altText:'A pair of socks',
-        inventory: 100
+        product: 'Socks',
+        image: 'vmSocks-green-onWhite.jpg',
+        altText: 'A pair of socks',
+        inventory: 100,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green"
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue"
+            }
+        ]
     }
 })
