8f8410315b4b61361f5e47c3033483bcd9060eda
Paul Pearce <chriton227@gmail.com>
2019-11-12 16:02:12 -0500

    Completed lesson 5

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # VueLearning
+
 A repo to store the exercise work from Intro to Vue.js course on VueMastery.com
--- main.js
+++ main.js
@@ -9,12 +9,23 @@ var app = new Vue({
         variants: [
             {
                 variantId: 2234,
-                variantColor: "green"
+                variantColor: 'green',
+                variantImage: 'vmSocks-green-onWhite.jpg'
             },
             {
                 variantId: 2235,
-                variantColor: "blue"
+                variantColor: 'blue',
+                variantImage: 'vmSocks-blue-onWhite.jpg'
             }
-        ]
+        ],
+        cart: 0
+    },
+    methods: {
+        addToCart: function() {
+            this.cart += 1;
+        },
+        updateProduct: function(variantImage) {
+            this.image = variantImage;
+        }
     }
 })
